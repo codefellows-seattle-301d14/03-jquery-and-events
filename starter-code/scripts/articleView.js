701c04fcbed67a71b4174061468520d27346628e
Hawa Abdi <abdih17@hotmail.com>
2016-10-26 22:39:34 -0700

    Cleaned up a little.

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -18,51 +18,54 @@ articleView.populateFilters = function() {
 };
 
 articleView.handleAuthorFilter = function() {
-    $('#author-filter').on('change', function() {
-        if ($(this).val()) {
-            /* TODO: If the slect box changes to an option that has a value, we should:
-            1. Hide all of the articles
-            2. Fade in only the articles that match based on on the author
-            that was aselected. Hint: use an attribute selector to find
-            those articles that match the value, and then fade them in.
-            */
-           $('article').hide();
-           var $authorConnector = ($(this).val());
-           console.log($authorConnector);
-            $('article[data-author="'+$authorConnector+'"]').fadeIn();
-        } else {
-            /* Otherwise, we should:
-            1. Show all the articles except the template */
-            $('article').not('.template').show();
-        }
-        $('#category-filter').val('');
-    });
+  $('#author-filter').on('change', function() {
+    if ($(this).val()) {
+      /* TODO: If the slect box changes to an option that has a value, we should:
+      1. Hide all of the articles
+      2. Fade in only the articles that match based on on the author
+      that was aselected. Hint: use an attribute selector to find
+      those articles that match the value, and then fade them in.
+      */
+      // DONE
+      $('article').hide();
+      var $authorConnector = ($(this).val());
+      console.log($authorConnector);
+      $('article[data-author="'+$authorConnector+'"]').fadeIn();
+    } else {
+      /* Otherwise, we should:
+      1. Show all the articles except the template */
+      $('article').not('.template').show();
+    }
+    $('#category-filter').val('');
+  });
 };
 
 articleView.handleCategoryFilter = function() {
   /* TODO: Just like we do for #author-filter above, we should also handle
   change events on the #category-filter element. Be sure to reset the
   #author-filter while you're at it! */
+  // DONE
   $('#category-filter').on('change', function(){
-     if ($(this).val()) {
-         $('article').hide();
-         var $categoryConnector = ($(this).val());
-         $('article[data-category="'+$categoryConnector+'"]').fadeIn();
-     }
-     else {
-        $('article').not('.template').show();
-     }
+    if ($(this).val()) {
+      $('article').hide();
+      var $categoryConnector = ($(this).val());
+      $('article[data-category="'+$categoryConnector+'"]').fadeIn();
+    }
+    else {
+      $('article').not('.template').show();
+    }
     $('#author-filter').val('');
-  })
+  });
 };
 
 articleView.handleMainNav = function () {
   $('.main-nav').on('click', '.tab', function() {
     /* TODO:
-      1. Hide all of the .tab-content sections
-      2. Fade in the single .tab-content section that is
-        associated with the .tab element's data-content attribute.
+    1. Hide all of the .tab-content sections
+    2. Fade in the single .tab-content section that is
+    associated with the .tab element's data-content attribute.
     */
+    // DONE
     $('.tab-content').hide();
     console.log($(this).attr('data-content'));
     var connector = $(this).attr('data-content');
@@ -75,26 +78,24 @@ articleView.setTeasers = function() {
   // Truncate logic to show only first two elements within the article body.
   $('.article-body *:nth-of-type(n+2)').hide();
   /* TODO: Add a delegated event handler to reveal the remaining paragraphs.
+  // DONE
 
-    When a .read-on link is clicked, we can:
-    1. Prevent the default action of a link.
-    2. Reveal everything in that particular article now.
-    3. Hide that read-on link!
+  When a .read-on link is clicked, we can:
+  1. Prevent the default action of a link.
+  2. Reveal everything in that particular article now.
+  3. Hide that read-on link!
+  // DONE!
 
-    // STRETCH GOAl!: change the 'Read On' link to 'Show Less'
+  // STRETCH GOAl!: change the 'Read On' link to 'Show Less'
   */
 
-
-$('#articles').on('click', 'a.read-on', function(event){
-
+  $('#articles').on('click', 'a.read-on', function(event){
     event.preventDefault();
-    console.log($(this));
     $('.article-body').show();
     $(this).parent().find('*').fadeIn();
+    console.log($(this));
     $(this).hide();
-    // $(this).show();
-    // $(this.read-on).hide();
-});
+  });
 };
 
 
