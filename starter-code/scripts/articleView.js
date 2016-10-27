bf055e13f3096ed16f9dfa7f250974fb89e05c56
regenalgrant <regenal@mac.com>
2016-10-26 17:12:28 -0700

    done line 22

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -25,13 +25,10 @@ articleView.handleAuthorFilter = function() {
             that was a selected. Hint: use an attribute selector to find
             those articles that match the value, and then fade them in.
         */
-        // for (var i = 0; i < allArticles.length; i++) {
-        // if (!allArticles[i].author===$(this).val()){
-        //     allArticles.splice(i, 1);
-        //   } allArticles.forEach(function(a){
-        //     $('#articles').fadeIn(a.toHtml());
-        //   });
-          console.log($(this).val());
+        // DONE
+        $('article').hide();
+        $('article[data-author="' + $(this).val() + '"]').fadeIn();
+
     } else {
     /* Otherwise, we should:
         1. Show all the articles except the template */
